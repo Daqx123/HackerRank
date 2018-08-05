eaa22747ce1fd393462104778a6d6dbcf7d15684
Dominik <dsadziak@users.noreply.github.com>
2018-08-05 19:02:24 +0100

    Add files via upload

---
+++ Jumping_on_the_Clouds.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int E = 100;
+        int c[] = new int[n];
+        for(int c_i=0; c_i < n; c_i++){
+            c[c_i] = in.nextInt();
+        }
+        
+        int i = 0;
+        
+        while(true){
+            if(c[i] == 0){
+                E-=1;
+                i = (i + k) % n;
+            } else if (c[i] == 1){
+                E-=3;
+                i = (i + k) % n;
+            }
+            if(i==0) break;
+        }
+        System.out.println(E);
+
+    }
+}
